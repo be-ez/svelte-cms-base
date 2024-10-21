1d10464acb1eed05c8dda1e939c594edf0e1812d
be-ez <a@xn-4qe.com>
2024-10-20 19:01:30 -0600

    add build dir

---
+++ build/_app/immutable/entry/start.py1X_abv.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D6Bt9Boq.js";export{t as start};
---
+++ build/_app/immutable/nodes/5.BlChMb4V.js
@@ -0,0 +1,1 @@
+import{a as o,t}from"../chunks/disclose-version.XZ5QMWem.js";var n=t("<span><p>Projects</p></span>");function r(a){var p=n();o(a,p)}export{r as component};
