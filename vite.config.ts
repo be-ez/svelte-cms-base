b63517016c804e001c7f01469eecf46c6406bf6b
be-ez <a@xn-4qe.com>
2025-03-13 22:54:41 -0600

    working

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		imagetools({
 			defaultDirectives: () => {
