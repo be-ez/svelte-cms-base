d07f40ad523de984356e82f6d041950590c90473
Alex Shulman <ajshulman@gmail.com>
2025-06-13 16:49:39 -0600

    markdown-in-recipes

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -4,11 +4,17 @@
 	import { Markdown } from 'svelte-exmarkdown';
 	import { gfmPlugin } from 'svelte-exmarkdown/gfm';
 
+	import { isDarkMode } from '$lib/stores/theme';
+
 	import DefaultPage from '../../../components/layout/defaultPage.svelte';
 
 	const plugins = [gfmPlugin()];
 </script>
 
 <DefaultPage title={data.recipe.title} subtitle={data.recipe.subtitle}>
-	<Markdown md={data.recipe.body} {plugins} />
+	<div class="max-w-4xl mx-auto py-12">
+		<div class="prose prose-lg mx-auto max-w-none" class:prose-invert={$isDarkMode}>
+			<Markdown md={data.recipe.body} {plugins} />
+		</div>
+	</div>
 </DefaultPage>
